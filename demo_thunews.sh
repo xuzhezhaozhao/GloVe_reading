21471ae0aae6fffd03c10077c8379de7c0932407
zhezhaoxu <zhezhaoxu@tencent.com>
2018-12-05 15:31:39 +0800

    format, add comment

--- demo_thunews.sh
+++ demo_thunews.sh
@@ -5,17 +5,7 @@ set -e
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
 
 make
-if [ ! -e text8 ]; then
-  if hash wget 2>/dev/null; then
-    wget http://mattmahoney.net/dc/text8.zip
-  else
-    curl -O http://mattmahoney.net/dc/text8.zip
-  fi
-  unzip text8.zip
-  rm text8.zip
-fi
-
-CORPUS=text8
+CORPUS=/home/xzz/work/fastText_reading/data/THUCNews/thucnews.2.preprocessed.all
 VOCAB_FILE=vocab.txt
 COOCCURRENCE_FILE=cooccurrence.bin
 COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
@@ -24,7 +14,7 @@ SAVE_FILE=vectors
 VERBOSE=2
 MEMORY=4.0
 VOCAB_MIN_COUNT=5
-VECTOR_SIZE=50
+VECTOR_SIZE=100
 MAX_ITER=15
 WINDOW_SIZE=15
 BINARY=2
@@ -40,14 +30,3 @@ echo "$ $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE
 $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
 echo "$ $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE"
 $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE -write-header 1
-
-if [ "$CORPUS" = 'text8' ]; then
-   if [ "$1" = 'matlab' ]; then
-       matlab -nodisplay -nodesktop -nojvm -nosplash < ./eval/matlab/read_and_evaluate.m 1>&2
-   elif [ "$1" = 'octave' ]; then
-       octave < ./eval/octave/read_and_evaluate_octave.m 1>&2
-   else
-       echo "$ python eval/python/evaluate.py"
-       # python eval/python/evaluate.py
-   fi
-fi
